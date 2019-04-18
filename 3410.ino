f202ae4164a3e8384291ac03bf75be4399b644f8
Laser Beard <therealmolotov@users.noreply.github.com>
2019-04-18 18:55:38 +0300

    Update 3410.ino

--- 3410.ino
+++ 3410.ino
@@ -3,29 +3,39 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-OneWire bus(A6);
+OneWire bus(A5);
 
 DallasTemperature sensors(&bus);
 
 #include "U8glib.h" 
 U8GLIB_PCF8812 u8g(7, 6, 4, 5, 3);   // SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9, Reset = 8
 
-float temperature;
-char temperatureString[6] = "-";
+float temperatureH;
+float temperatureS;
+char temperatureStringH[6] = "-";
+char temperatureStringS[6] = "-";
+
 
 void draw(void) {
 // graphic commands to redraw the complete screen should be placed here
-u8g.setFont(u8g_font_unifont);
-u8g.drawStr( 0, 22, "In room:");
-u8g.setFont(u8g_font_fub25);
-// u8g.setFont(u8g_font_osr);
-u8g.drawStr( 0, 54, temperatureString);
-u8g.drawStr( 100, 54, "C");
+u8g.setFont(u8g_font_unifont_0_8);
+u8g.drawStr( 0, 10, "Outdoor:");
+//u8g.setFont(u8g_font_fub25);
+u8g.setFont(u8g_font_8x13B);
+u8g.drawStr( 0, 27, temperatureStringH);
+u8g.drawStr( 43, 27, "\260C");
+u8g.setFont(u8g_font_unifont_0_8);
+u8g.drawStr( 0, 43, "Indoor:");
+u8g.setFont(u8g_font_8x13B);
+u8g.drawStr( 0, 58, temperatureStringS);
+u8g.drawStr( 43, 58, "\260C");
 }
 
 void setup() {
 // Start the sensors
-u8g.setRot180();
+
+// Rotate the screen
+// u8g.setRot180();
 
 sensors.begin();
 }
@@ -37,12 +47,13 @@ do {
 draw();
 } while( u8g.nextPage() );
 
-// Update the sensor readings
+// Read Temperature
 sensors.requestTemperatures();
 
-// Read Temperature
-temperature = sensors.getTempCByIndex(0);
-dtostrf(temperature, 2, 1, temperatureString);
+temperatureH = sensors.getTempCByIndex(0);
+dtostrf(temperatureH, 2, 2, temperatureStringH);
+temperatureS = sensors.getTempCByIndex(1);
+dtostrf(temperatureS, 2, 2, temperatureStringS);
 
 // Delay for 1 seconds between readings
 delay(1000);
